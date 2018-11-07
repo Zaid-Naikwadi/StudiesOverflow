dcfeae23feb4718774a351d4fadcb054a2411a7f
Zaid Naikwadi <naikwadizaid@gmail.com>
2018-11-08 01:06:22 +0530

    Add files via upload

---
+++ VitOverflow/index/signup.php
@@ -0,0 +1,51 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "vitoverflow";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+extract($_POST);
+
+
+
+$rs=mysqli_query($conn,"select * from user_db where email='$u_email'");
+if(mysqli_num_rows($rs)>0)
+{
+
+?>
+<script type="text/javascript">
+alert("User Already Exists, please use another Email id");
+window.location.href = "index.html";
+</script>
+<?php
+
+}
+else
+{
+
+
+$sql = "INSERT INTO user_db (user_name, email, password, department, user_type)
+VALUES ('$u_name', '$u_email', '$u_password', '$u_department', '1')";
+
+if ($conn->query($sql) === TRUE) {
+    ?>
+	<script type="text/javascript">
+	alert("You Have successfully registered,you can use your credentials for Login");
+	window.location.href = "../login/login.html";
+	</script>
+	<?php
+  	exit();
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+}
+
+$conn->close();
+?>
